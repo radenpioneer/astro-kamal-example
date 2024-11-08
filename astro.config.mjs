2dc0401f37d298f988e1c3eec0454b4cd7812bd4
R. Ilham Sastronegoro <7355835+radenpioneer@users.noreply.github.com>
2024-11-08 08:24:32 +0000

    setup prettier

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,8 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
-import react from '@astrojs/react';
-import keystatic from '@keystatic/astro';
-import node from '@astrojs/node';
+import { defineConfig } from 'astro/config'
+import react from '@astrojs/react'
+import keystatic from '@keystatic/astro'
+import node from '@astrojs/node'
 
 // https://astro.build/config
 export default defineConfig({
@@ -11,4 +11,4 @@ export default defineConfig({
   adapter: node({
     mode: 'standalone'
   })
-});
+})
